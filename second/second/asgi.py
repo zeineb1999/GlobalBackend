9d3360a934add300ee78bee9cfe19946f80092de
zeineb araar <zeineb.ara@hotmail.fr>
2024-05-20 23:21:34 +0100

    init

---
+++ second/second/asgi.py
@@ -0,0 +1,31 @@
+
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
+
+application = get_asgi_application()
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from taches.urls import websocket_urlpatterns  # Importez les routes WebSocket de votre application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
+
+# Obtenez l'application ASGI de Django
+django_asgi_application = get_asgi_application()
+
+# Définissez le routeur des protocoles pour traiter les connexions HTTP et WebSocket
+application = ProtocolTypeRouter({
+    "http": django_asgi_application,  # Traitez les connexions HTTP avec l'application Django standard
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+          websocket_urlpatterns  # Utilisez les routes WebSocket définies dans votre application
+        )
+    ),
+})
+ 
---
+++ tutorial/quickstart/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Zone)
+admin.site.register(Equipement)
+admin.site.register(Etage)
+admin.site.register(ExcelData)
+admin.site.register(Batiment)
+admin.site.register(PeriodeActivite)
+admin.site.register(Alerte)
+admin.site.register(ProfileUser)
+admin.site.register(Rapport)
+admin.site.register(Sauvegarde)
+admin.site.register(Historique)
+
+from rest_framework.authtoken.admin import TokenAdmin
+
+TokenAdmin.raw_id_fields = ['user']
+
+
